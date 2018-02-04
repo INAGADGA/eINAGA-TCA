6e1f4c3bbec601983333fe47ec8a406f7a00b3a5
rdonate@gmail.com <rdonate@gmail.com>
2018-02-04 18:09:19 +0100

    setfechas

--- ios/www/scripts/cotos.js
+++ ios/www/scripts/cotos.js
@@ -531,7 +531,6 @@ require([
             var dia = d.getDate();
             var mes = d.getMonth() + 1;
             var diaIni = dia;
-            var anio = d.getFullYear();
 
             if (mes === 2) { // bisiesto
                 var bisiesto = (anio % 4 === 0 && (anio % 100 !== 0 || anio % 400 === 0));
